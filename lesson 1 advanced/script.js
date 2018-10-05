79f4d0de72591131231d295ac0f35f7259760662
Renat <hustlersteve@gmail.com>
2018-10-05 15:40:01 +0300

    commit 6

--- lesson 1 advanced/script.js
+++ lesson 1 advanced/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
 let num = "33721",
     arr = num.split(""),
     sum = 1;
@@ -13,18 +12,3 @@ arr.forEach(function(item, i, arr) {
 sum = sum  ** 3;
 
 alert(sum.toString().slice(0, 3));
-=======
-let num = "33721";
-let arr = num.split("");
-
-let i,
-    summ = 1;
-for (i = 0; i < arr.length; i++) { 
-    summ = summ * parseInt(arr[i]);
-}
-
-summ = summ  ** 3;
-summ = summ.toString();
-
-alert(summ.slice(0, 3));
->>>>>>> b7da6bd2eb9f0ccacd0ef09068c22bd197a9dcb0
