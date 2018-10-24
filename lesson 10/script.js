5cab185386396a8801173bfb4394c444845726a4
Renat <hustlersteve@gmail.com>
2018-10-24 16:21:16 +0300

    lesson 10

--- lesson 10/script.js
+++ lesson 10/script.js
@@ -2,10 +2,10 @@
 
 // Создать класс options +
 // ·        Он должен содержать свойства: height, width, bg, fontSize, textAlign +
-// ·        Он должен содержать метод, создающий новый div на странице, 
-// записывающий в него любой текст и при помощи cssText изменять свой стиль из переданных параметров
-// ·        Создать новый объект через класс
-// ·        Вызвать его метод и получить элемент на странице
+// ·        Он должен содержать метод, создающий новый div на странице, +
+// записывающий в него любой текст и при помощи cssText изменять свой стиль из переданных параметров +
+// ·        Создать новый объект через класс + 
+// ·        Вызвать его метод и получить элемент на странице +
 
 class Options {
     constructor(height, width, bg, fontSize, textAlign) {
@@ -14,17 +14,17 @@ class Options {
       this.bg = bg;
       this.fontSize = fontSize;
       this.textAlign = textAlign;
+      this.elm = document.createElement('div');
     }
     newDiv(style, text) {
-        let elm = document.createElement('div'),
-            txt = document.createTextNode(text);
-        elm.appendChild(txt);  
-        elm.style.cssText = `height:${this.height}px; width:${this.width}px; 
+        let txt = document.createTextNode(text);
+        this.elm.appendChild(txt);  
+        this.elm.style.cssText = `height:${this.height}px; width:${this.width}px; 
             background-color: ${this.bg}; font-size: ${this.fontSize}px; text-align:${this.textAlign};` + style;
-        document.body.appendChild(elm);
+        document.body.appendChild(this.elm);
       }
   }
 
   let element = new Options('100', '300', 'orange', '15', 'center');
   element.newDiv("background-color:pink;", 'testing');
-  element.newDiv("text-align: right;", 'second one');
+  element.newDiv("text-align: left;", 'added');
